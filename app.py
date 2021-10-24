0e6ba90d9bcca2ab5a64c527bc02c58b8e3f121f
ErlingHW <erlingstaff@gmail.com>
2021-10-24 17:08:44 +0200

    compete example of app.py. gitignore added

--- app.py
+++ app.py
@@ -1,7 +1,43 @@
 import cv2
-import deepface
+from deepface import DeepFace
+import matplotlib.pyplot as plt
+import numpy as np
+
+face_cascade = cv2.CascadeClassifier("cascade.xml")
+cap = cv2.VideoCapture("video.mp4")
+itt = 0
+
+while(cap.isOpened()):
+    itt+=1
+    ret, frame = cap.read()
+    if itt % 5 == 0:
+            
+        if ret:
+            
+            prediction = DeepFace.analyze(frame, actions=["emotion"])
+
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+            faces = face_cascade.detectMultiScale(gray, 1.1, 15)
+
+            #print(prediction)
+
+            for (x, y, w, h) in faces:
+                cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+            font = cv2.FONT_HERSHEY_COMPLEX
+
+            cv2.putText(frame, prediction["dominant_emotion"], (x-150, y), font, 2, (0, 255, 0), 2, cv2.LINE_4)
+            
+            ims = cv2.resize(frame, (960, 960))
+
+            cv2.imshow('Frame', ims)
+            if cv2.waitKey(25) & 0xFF == ord('q'):
+                break
+
+        else:
+            break
 
 
 
-
-
+cap.release()
+cv2.destroyAllWindows()

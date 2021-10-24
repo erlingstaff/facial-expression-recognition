2874d19043ec3930e657525bdbe529b4f5744fc0
ErlingHW <erlingstaff@gmail.com>
2021-10-24 17:13:34 +0200

    removed unecesarry imports

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
 import cv2
 from deepface import DeepFace
-import matplotlib.pyplot as plt
-import numpy as np
 
 face_cascade = cv2.CascadeClassifier("cascade.xml")
 cap = cv2.VideoCapture("video.mp4")

ebbb520eb2004c4785007443146af2d0326394ce
ErlingHW <erlingstaff@gmail.com>
2021-10-24 18:02:18 +0200

    performance boost

--- app.py
+++ app.py
@@ -2,24 +2,25 @@ import cv2
 from deepface import DeepFace
 
 face_cascade = cv2.CascadeClassifier("cascade.xml")
-cap = cv2.VideoCapture("video.mp4")
+cap = cv2.VideoCapture("stest.mp4")
 itt = 0
 
 while(cap.isOpened()):
     itt+=1
     ret, frame = cap.read()
-    if itt % 5 == 0:
+    if itt % 10 == 0:
             
         if ret:
             
-            prediction = DeepFace.analyze(frame, actions=["emotion"])
             gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
             faces = face_cascade.detectMultiScale(gray, 1.1, 15)
+            
             for (x, y, w, h) in faces:
-                cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
-            font = cv2.FONT_HERSHEY_COMPLEX
-
-            cv2.putText(frame, prediction["dominant_emotion"], (x-150, y), font, 2, (0, 255, 0), 2, cv2.LINE_4)
+                prediction = DeepFace.analyze(frame, actions=["emotion"], enforce_detection=False)
+                if prediction["emotion"]["neutral"] < 5:
+                    cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+                    font = cv2.FONT_HERSHEY_COMPLEX            
+                    cv2.putText(frame, prediction["dominant_emotion"], (150, 150), font, 2, (0, 255, 0), 2, cv2.LINE_4)
             
             ims = cv2.resize(frame, (960, 960))
 
